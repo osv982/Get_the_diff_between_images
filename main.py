136905fd0cacfa104f86c0f5a1e194c639bb4987
osv982 <serezhenka_oleynikov@bk.ru>
2022-06-11 00:58:00 +0300

    delete foo() соотношения сторон, состояние перед загрузкой пикселей в массив

--- main.py
+++ main.py
@@ -4,48 +4,30 @@ import cv2
 
 
 def diff():
+
+    img_1 = "image_1.jpg"
+    img_2 = "image_2.jpg"
     # открываем изображения
     try:
-        image_one = Image.open("image_1.jpg")
-        image_two = Image.open("image_2.jpg")
+        image_one = cv2.imread(img_1)
+        image_two = cv2.imread(img_2)
     except FileNotFoundError:
         print("Файл не найден")
 
-    # конвертируем в серый
-    img_gray_1 = cv2.imread("image_1.jpg", 0)  # если не сработает то вместо image_one two их явные имена
-    img_gray_2 = cv2.imread("image_2.jpg", 0)
+    # конвертация в grayscale
+    img_gray_1 = cv2.imread(img_1, 0)
+    img_gray_2 = cv2.imread(img_2, 0)
 
-    resizing(img_gray_1, 16, 16)
-    resizing(img_gray_2, 16, 16)
-
-
-
-
-# конвертируем в нужный размер
-def resizing(img, new_width=None, new_height=None, interp=cv2.INTER_LINEAR):
-    h, w = img.shape[:2]
-
-    if new_width is None and new_height is None:
-        return img
-
-    if new_width is None:
-        ratio = new_height / h
-        dimension = (int(w * ratio), new_height)
-
-    else:
-        ratio = new_width / w
-        dimension = (new_width, int(h * ratio))
-
-    res_img = cv2.resize(img, dimension, interpolation=interp)
+    # преобразование изображения в нужный размер
+    res_img_1 = cv2.resize(img_gray_1, (16, 16), cv2.INTER_NEAREST)
+    res_img_2 = cv2.resize(img_gray_2, (16, 16), cv2.INTER_NEAREST)
 
     # To display Image
     window_name = 'Grayscale Conversion OpenCV'
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(window_name,  res_img)  # вывожу только одно изображение
+    cv2.imshow(window_name, res_img_1)  # вывожу только одно изображение
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-    # return res_img
 
 
 if __name__ == '__main__':
--- ocv.py
+++ ocv.py
@@ -8,24 +8,24 @@ img_gray = cv2.imread("image_1.jpg", 0)
 # img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
 
 # преобразование изображения в нужный размер
-res_img = cv2.resize(img_gray, (500, 900), cv2.INTER_NEAREST)
+res_img = cv2.resize(img_gray, (16, 16), cv2.INTER_NEAREST)
 
-# def resizing(new_width=None, new_height=None, interp=cv2.INTER_LINEAR):
-h, w = img_gray.shape[:2]
-new_width, new_height = 16
-
-# if new_width is None and new_height is None:
-# return img_gray
-
-if new_width is None:
-    ratio = new_height / h
-    dimension = (int(w * ratio), new_height)
-
-else:
-    ratio = new_width / w
-    dimension = (new_width, int(h * ratio))
-
-res_img = cv2.resize(img, dimension, interpolation=interp)
+# # def resizing(new_width=None, new_height=None, interp=cv2.INTER_LINEAR):
+# h, w = img_gray.shape[:2]
+# new_width, new_height = 16
+#
+# # if new_width is None and new_height is None:
+# # return img_gray
+#
+# if new_width is None:
+#     ratio = new_height / h
+#     dimension = (int(w * ratio), new_height)
+#
+# else:
+#     ratio = new_width / w
+#     dimension = (new_width, int(h * ratio))
+#
+# res_img = cv2.resize(img, dimension, interpolation=interp)
 
 # To display Image
 window_name = 'Grayscale Conversion OpenCV'
