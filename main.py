106f8568883cf8af5d8741de33a1f38b2b9ef2e1
osv982 <serezhenka_oleynikov@bk.ru>
2022-06-12 18:08:23 +0300

    нашли процент

--- main.py
+++ main.py
@@ -22,9 +22,13 @@ def diff():
     img_gray_1 = cv2.imread(img_1, 0)
     img_gray_2 = cv2.imread(img_2, 0)
 
+    dimension = (16, 16)
+    amount_pixels = dimension[0] * dimension[1]
+    print(f"размер изображения: {amount_pixels} пикселей")
+
     # преобразование изображения в нужный размер  (столбец, строка)   print(len(numpydata_2)) дает количетсво строк
-    res_img_1 = cv2.resize(img_gray_1, (16, 16), cv2.INTER_NEAREST)
-    res_img_2 = cv2.resize(img_gray_2, (16, 16), cv2.INTER_NEAREST)
+    res_img_1 = cv2.resize(img_gray_1, dimension, cv2.INTER_NEAREST)
+    res_img_2 = cv2.resize(img_gray_2, dimension, cv2.INTER_NEAREST)
 
     # считывание пикселей в массив
     numpydata_1 = asarray(res_img_1)
@@ -32,15 +36,23 @@ def diff():
     print(numpydata_1, '\n')
     print(numpydata_2)
 
-    threshold = 5
+    threshold = int(input('введите пороговое значение [0;255]\n'))
     count_dif_pix = 0
 
     for i in range(len(numpydata_1)):
         for j in range(len(numpydata_1)):
-            if abs(int(numpydata_1[i][j]) - int(numpydata_2[i][
-                                                    j])) > threshold:  # т к пиксели хранятся в uint8 от 0 до 255, то возникает исключение, поэтому приведем к int
+            # т к пиксели хранятся в uint8 от 0 до 255, то возникает исключение, поэтому приведем к int
+            if abs(int(numpydata_1[i][j]) - int(numpydata_2[i][j])) > threshold:
                 count_dif_pix += 1
     print(f"количество различных пикселей {count_dif_pix}")
+    print(f"размер изображения: {amount_pixels} пикселей")
+
+    per_dif = (count_dif_pix / amount_pixels) * 100
+
+    if round(per_dif) == 0:
+        print("изображения одинаковые")
+    else:
+        print(f"изображения различаются на {round(per_dif)} % ")
 
     #
     # # To display Image
