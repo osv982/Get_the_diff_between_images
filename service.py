bfdc748bfeb234ba0d3c3cc9057a85da9bc816df
osv982 <78878728+osv982@users.noreply.github.com>
2022-06-13 23:50:36 +0300

    Update service.py

--- service.py
+++ service.py
@@ -1,18 +1,18 @@
-def my_input():
+def my_input(msg_welcome: str, msg_TypeError: str, msg_ValueError: str):
     # валидация ввода
     successful_input = False
     while not successful_input:
         try:
-            buffer = (input('Введите пороговое значение на отрезке [0;255]\n'))
+            buffer = (input(msg_welcome))
             value = float(buffer)
             if my_range(value, 0, 255):
                 successful_input = True
             else:
                 raise ValueError
         except TypeError:
-            print("Неверный тип. Попробуйте снова")
+            print(msg_TypeError)
         except ValueError:
-            print("Значение некорректно. Попробуйте снова")
+            print(msg_ValueError)
     return value
 
 
